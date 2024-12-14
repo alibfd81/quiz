8e2df21b335b0b027e0719047bc24b1aa8c12e44
alibfd81 <alibfd810@gmail.com>
2024-12-14 22:38:57 +0330

    change code

--- src/features/Quiz/Questions.jsx
+++ src/features/Quiz/Questions.jsx
@@ -6,7 +6,7 @@ import TotalScore from "./TotalScore";
 function Questions({ currentQuestion, onClickNext, handlePrev }) {
     const [timeleft, setTimeLeft] = useState(() => {
         const savedTime = localStorage.getItem("timeLeft");
-        return savedTime ? JSON.parse(savedTime) : 10;
+        return savedTime ? JSON.parse(savedTime) : 300;
     });
 
     // بارگذاری پاسخ‌ها از localStorage در زمان رندر کامپوننت
